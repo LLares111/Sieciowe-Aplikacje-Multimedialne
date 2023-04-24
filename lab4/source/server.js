0809e0356a4aed248fcf7f0b89b4f01c2471985e
LLares111 <44227836+LLares111@users.noreply.github.com>
2023-04-24 14:28:48 +0200

    removeRow function added

--- lab4/source/server.js
+++ lab4/source/server.js
@@ -1,16 +1,12 @@
+
 const express = require('express')
 
 const app = express();
-
-
 app.get('/', (req, res) => {
     let video="", audio="", image="";
-    if (req.query.videoFile !== null)
-        video = req.query.videoFile;
-    if (req.query.audioFile !== null)
-        audio = req.query.audioFile;
-    if (req.query.imgFile !== null)
-        image = req.query.imgFile;
+    if (req.query.videoFile !== null) video = req.query.videoFile;
+    if (req.query.audioFile !== null) audio = req.query.audioFile;
+    if (req.query.imgFile !== null) image = req.query.imgFile;
     let html = `
 <!DOCTYPE html>
 <script>
@@ -19,53 +15,61 @@ app.get('/', (req, res) => {
     if (isAudioVis === "" || isAudioVis === "hidden")
         document.getElementById("audioAdd").style.visibility = "hidden";
     let isVideoVis = document.getElementById("videoCancel").style.visibility;
-    if (isVideoVis==="" || isVideoVis === "hidden")
+    if (isVideoVis === "" || isVideoVis === "hidden")
         document.getElementById("videoAdd").style.visibility = "hidden";
     };
-    function videoCancelFunction() {
-    	const tmp = document.getElementById("videoPlayer");
-    	tmp.src = "cancel.mp4";
-    }
-    function audioCancelFunction() {
-        const tmp = document.getElementById("audioPlayer");
-        tmp.src = "cancel.mp3";
+    function cancelFunction(type) {
+    	if(type == 0) {
+    		const tmp = document.getElementById("videoPlayer");
+    		tmp.src = "cancel.mp4";
+    	}
+    	else if (type == 1) {
+    		const tmp = document.getElementById("audioPlayer");
+        	tmp.src = "cancel.mp3";
+    	}
+    	else if (type == 2) {
+    		const tmp = document.getElementById("posterImage");
+        	tmp.src = "cancel.jpg";
+    	}
     }
     var rows = 0;
-    function addAudioRow() {
+    
+    function addRow(type) { // 0 for video, 1 for audio, 2 for image
     	rows = rows + 1;
-        var table = document.getElementById("table");
+    	var table = document.getElementById("playlist_table");
         var row = table.insertRow(rows);
         row.insertCell(0).innerHTML = rows;
-        row.insertCell(1).innerHTML = document.getElementById("audioPlayer").getAttribute("src");;
-        row.insertCell(2).innerHTML = "Audio";
+        if(type == 0) {
+        	row.insertCell(1).innerHTML = document.getElementById("videoPlayer").getAttribute("src");
+        	row.insertCell(2).innerHTML = "Video";
+        }
+        else if(type == 1) {
+        	row.insertCell(1).innerHTML = document.getElementById("audioPlayer").getAttribute("src");
+        	row.insertCell(2).innerHTML = "Audio";
+        }
+        else if(type == 2) {
+        	row.insertCell(1).innerHTML = document.getElementById("posterImage").getAttribute("src");
+        	row.insertCell(2).innerHTML = "Image";
+        }
     }
-    function addVideoRow() {
-    	rows = rows + 1;
-        var table = document.getElementById("table");
-        var row = table.insertRow(rows);
-        row.insertCell(0).innerHTML = rows;
-        row.insertCell(1).innerHTML = document.getElementById("videoPlayer").getAttribute("src");;
-        row.insertCell(2).innerHTML = "Video";
+    
+    function removeRow(row) {
+    	var table = document.getElementById("playlist_table");
+    	var current = table.deleteRow(row);
+    	rows = rows - 1;
     }
-    function addImageRow() {
-    	rows = rows + 1;
-        var table = document.getElementById("table");
-        var row = table.insertRow(rows);
-        row.insertCell(0).innerHTML = rows;
-        row.insertCell(1).innerHTML = document.getElementById("posterImage").getAttribute("src");;
-        row.insertCell(2).innerHTML = "Image";
-    }
+    
 </script>
-<html lang="pl">
+<html>
     <video id="videoPlayer" width="320" height="240" controls src=${video}></video> <br>
     	<audio id="audioPlayer" controls src=${audio}></audio> <br>
     	<img id="posterImage" src=${image}> <br>
-        <button type="button" id="videoCancel" onclick="videoCancelFunction()"  style="visibility: visible">Video to Cancel</button> <br>
-        <button type="button" id="audioCancel" onclick="audioCancelFunction()" style="visibility: visible">Audio to Cancel</button> <br>
-        <button type="button" id="audioAdd" onclick="addAudioRow()">Audio add</button> <br>
-        <button type="button" id="videoAdd" onclick="addVideoRow()">Video add</button> <br>
-        <button type="button" id="imageAdd" onclick="addImageRow()">Image add</button> <br>
-        <table id="table">
+        <button type="button" id="videoCancel" onclick="cancelFunction(0)"  style="visibility: visible">Video to Cancel</button>
+        <button type="button" id="videoAdd" onclick="addRow(0)">Add video</button> <br>
+        <button type="button" id="audioCancel" onclick="cancelFunction(1)" style="visibility: visible">Audio to Cancel</button>
+        <button type="button" id="audioAdd" onclick="addRow(1)">Add audio</button> <br>
+        <button type="button" id="imgAdd" onclick="addRow(2)">Add image</button> <br>
+        <table id="playlist_table">
         	<tr>
         		<th>No.</th>
         		<th>URL</th>
@@ -76,7 +80,5 @@ app.get('/', (req, res) => {
 </html>
 `
     res.send(html)
-
 })
-
 app.listen(4080)
